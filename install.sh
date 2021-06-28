4891621465d5dce6729e32aebbb2fba564431291
M4RC0Sx <marcos3as3@gmail.com>
2021-06-28 23:53:07 +0200

    Fixed manual symlinks creation.

--- install.sh
+++ install.sh
@@ -16,9 +16,9 @@ mkdir -p ~/Images/Screenshot
 
 # Generate X11 symlinks - sudo needed.
 sudo mkdir -p /etc/X11/xorg.conf.d
-sudo ln -sf ./_X11/xorg.conf.d/* /etc/X11/xorg.conf.d/
+sudo ln -sf "$(pwd)"/_X11/xorg.conf.d/* /etc/X11/xorg.conf.d/
 
 # Generate JRE profile file - sudo needed.
 sudo mkdir -p /etc/profile.d
-sudo ln -sf ./_profile.d/jre.sh /etc/profile.d
+sudo ln -sf "$(pwd)"/_profile.d/jre.sh /etc/profile.d
 
