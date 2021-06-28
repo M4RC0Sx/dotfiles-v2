8ac1ba3113e7684bc5a9b02a2ac7bbce887ddb6b
M4RC0Sx <marcos3as3@gmail.com>
2021-06-28 15:10:37 +0200

    Updated install script.

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
+# Generate stow symlinks.
 for file in *; do
-    if [ -d "$file" ]; then
+    if [ -d "$file" ] && [[ "$file" != _* ]]; then
         echo "Generating $file symlinks via stow..."
         stow "$file"
     fi
 done
+
+# Generate X11 symlinks - sudo needed.
+sudo mkdir -p /etc/X11/xorg.conf.d
+sudo ln -sf ./_X11/xorg.conf.d/* /etc/X11/xorg.conf.d/
+
+# Generate JRE profile file - sudo needed.
+sudo mkdir -p /etc/profile.d
+sudo ln -sf ./_profile.d/jre.sh /etc/profile.d
+
