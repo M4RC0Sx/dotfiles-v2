fdaaeb6852b6578e6af370b443c1fd9a0b7cdd8e
M4RC0Sx <marcos3as3@gmail.com>
2021-07-17 09:52:12 +0200

    Fixed IP display.

--- polybar/.config/polybar/ip.sh
+++ polybar/.config/polybar/ip.sh
@@ -4,7 +4,7 @@
 E=$(ip addr show dev enp0s20f0u3u1 2>/dev/null | wc -l)
 W=$(ip addr show dev wlp2s0 | wc -l)
 
-if [[ "$E" -gt 2 && "$W" -lt 2 ]]; then
+if [[ "$E" -gt 2 && "$W" -lt 3 ]]; then
 	echo -e " $(ip addr show dev enp0s20f0u3u1 2>/dev/null| grep "inet " | awk '{print $2}')%{u-}"
 elif [[ "$W" -gt 2 && "$E" -lt 2 ]]; then
 	echo -e " $(ip addr show dev wlp2s0 2>/dev/null| grep "inet " | awk '{print $2}')%{u-}"
