90a289bfaea07bf666e9e505c4a69966ab30a13f
M4RC0Sx <marcos3as3@gmail.com>
2021-09-13 16:19:45 +0200

    Update polybar network interfaces.

--- polybar/.config/polybar/ip.sh
+++ polybar/.config/polybar/ip.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Ethernet & WiFi
-E=$(ip addr show dev enp0s20f0u3u1 2>/dev/null | wc -l)
-W=$(ip addr show dev wlp2s0 | wc -l)
+E=$(ip addr show dev eth0 2>/dev/null | wc -l)
+W=$(ip addr show dev wlan0 | wc -l)
 
 if [[ "$E" -gt 2 && "$W" -lt 3 ]]; then
-	echo -e " $(ip addr show dev enp0s20f0u3u1 2>/dev/null| grep "inet " | awk '{print $2}')%{u-}"
+	echo -e " $(ip addr show dev eth0 2>/dev/null| grep "inet " | awk '{print $2}')%{u-}"
 elif [[ "$W" -gt 2 && "$E" -lt 2 ]]; then
-	echo -e " $(ip addr show dev wlp2s0 2>/dev/null| grep "inet " | awk '{print $2}')%{u-}"
+	echo -e " $(ip addr show dev wlan0 2>/dev/null| grep "inet " | awk '{print $2}')%{u-}"
 elif [[ "$W" -gt 2 && "$E" -gt 2 ]]; then
-	echo -e " $(ip addr show dev enp0s20f0u3u1 2>/dev/null| grep "inet " | awk '{print $2}')%{u-}   $(ip addr show dev wlp2s0 2>/dev/null| grep "inet " | awk '{print $2}')%{u-}"
+	echo -e " $(ip addr show dev eth0 2>/dev/null| grep "inet " | awk '{print $2}')%{u-}   $(ip addr show dev wlan0 2>/dev/null| grep "inet " | awk '{print $2}')%{u-}"
 else
 	echo -e ""
 fi
